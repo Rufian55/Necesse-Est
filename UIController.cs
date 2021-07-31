808be21f41a83323659493527d478d76997dccbc
Rufian55 <chriskearns@rigidbody.tech>
2021-07-31 16:36:36 -0500

    More to come...

---
+++ UIController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }    
+    }
+
+}

366d52373b943bc08c146de53f8d77641eac6abe
Rufian55 <chriskearns@rigidbody.tech>
2021-07-31 16:35:53 -0500

    More to come...

---
+++ CameraOrbitor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CameraOrbitor : MonoBehaviour {
+
+    public Transform camRotatesAroundThis;          // Named "DeadCenter" in heirarchy.
+    private float orbitSpeed = 5f;
+    private Vector3 orbitAxis;                      // Used for: primary axis of camera rotation, enable right mouse click User override.
+    private float xMouseStart = 0;                  // Used to capture User's Right Mouse up/down Input.
+   // public Vector3 offset;                          // Set in Inspector as (25,75,25) - not implemented, but can be used to zoom, etc.
+    private Vector3 relativePosition;               // Used to hold intermediate position data as Camera transitions to new star.
+    private Quaternion newRotation;                 // Used to hold intermediate rotation data as Camera "Look Rotates".
+    private Vector3 axis = new Vector3(1, 1f, 0);
+    void Start() {
+        orbitAxis = axis;
+    }
+
+    void LateUpdate() {
+        if (Input.GetMouseButtonDown(0)) {
+            orbitAxis = axis;
+        }
+        if (Input.GetMouseButtonDown(1)) {                                                                              // RH Mouse down!
+            xMouseStart = Input.mousePosition.x;
+        }
+        if (Input.GetMouseButtonUp(1)) {                                                                                // RH Mouse up!
+            xMouseStart = 0;
+        }
+        // Enable User to manually control speed and direction of simulation rotation.
+        if (Input.GetMouseButton(1)) {                                                                                  // User is holding RH Down!
+            orbitAxis = Input.mousePosition.x > xMouseStart ? Vector3.down : Vector3.up;                                // Get mouse direction.
+            orbitSpeed = Mathf.Clamp(Mathf.Abs(Input.mousePosition.x - xMouseStart), 0, 10f);                           // Clamp rotation speed.
+            transform.RotateAround(camRotatesAroundThis.transform.position, orbitAxis, orbitSpeed * Time.deltaTime);    // Rotate on orbitAxis per User Input.
+        }
+        else { // Otherwise, "auto" track and rotate Camera.
+            // Vector subtraction get intermediate camera position.
+            relativePosition = camRotatesAroundThis.position - transform.position;
+
+            // Establish the new rotation by "looking" at the new intermediate position. 
+            newRotation = Quaternion.LookRotation(relativePosition);
+
+            // Execute the new "incremental" rotation against the Camera's transform.
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, orbitSpeed * Time.deltaTime);
+
+            // Add the overall rotation of the camera on its axis at a frame rate adjusted speed.
+            transform.RotateAround(camRotatesAroundThis.transform.position, orbitAxis, orbitSpeed * Time.deltaTime);
+        }
+    }
+
+}

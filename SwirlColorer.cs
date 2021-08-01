2b5da2f4c881fd9228e661b3569c38af1cff10d2
Rufian55 <chriskearns@rigidbody.tech>
2021-08-01 15:14:45 -0500

    Work in Progress!

---
+++ SwirlColorer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SwirlColorer : MonoBehaviour {
+
+    public static SwirlColorer Instance = null;
+    [SerializeField] private Color[] swirlColorsLow;
+    [Space]
+    [SerializeField] private Color[] swirlColorsHigh;
+
+    private int arg;
+    private ParticleSystem swirls;
+    private ParticleSystem.MainModule settings;
+
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        swirls = GetComponent<ParticleSystem>();
+        settings = swirls.main;
+    }
+
+    public void SetSwirlColors () {
+        arg = ObjectAccessor.Instance.SkyBoxIndex;
+        settings.startColor = new ParticleSystem.MinMaxGradient(swirlColorsLow[arg], swirlColorsHigh[arg]);
+    }
+
+}

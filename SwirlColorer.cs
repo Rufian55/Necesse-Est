0c86110527c90f1a3a164d8747e9bb9ec4d8efcd
Rufian55 <chriskearns@rigidbody.tech>
2021-08-02 10:13:48 -0500

    Work in Progress!

--- SwirlColorer.cs
+++ SwirlColorer.cs
@@ -1,11 +1,20 @@
-﻿using UnityEngine;
+﻿/**********************************************************************************
+ * SwirlColorer.cs - manages runtime assignment of color gradientsto swirl particle
+ * system.  Note new gradient startcolor assignment changes the PS, not the Shader.
+ * [Seperate class from ObjectAccessor due to issues with maintaining runtime 
+ *  references to the particle system.]
+ *********************************************************************************/
+using UnityEngine;
 
 public class SwirlColorer : MonoBehaviour {
 
     public static SwirlColorer Instance = null;
+
+    #pragma warning disable 0649
     [SerializeField] private Color[] swirlColorsLow;
     [Space]
     [SerializeField] private Color[] swirlColorsHigh;
+    #pragma warning restore 0649
 
     private int arg;
     private ParticleSystem swirls;

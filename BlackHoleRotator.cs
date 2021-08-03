afca62c2365dba9f31634855b57d4bd7d379fd2a
Rufian55 <chriskearns@rigidbody.tech>
2021-08-03 16:34:54 -0500

    New file

---
+++ BlackHoleRotator.cs
@@ -0,0 +1,17 @@
+﻿/***********************************************************************************
+ * BlackHoleRotator.cs - simple rotation script.
+ ***********************************************************************************/
+using UnityEngine;
+
+public class BlackHoleRotator : MonoBehaviour {
+    
+    #pragma warning disable 0649
+    [SerializeField] private GameObject coreAssembly;
+    [SerializeField] private float rotationSpeed;
+    #pragma warning restore 0649
+
+    void FixedUpdate() {
+        coreAssembly.transform.Rotate(0f, rotationSpeed * Time.fixedDeltaTime, 0f);
+    }
+
+}

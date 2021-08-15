37f6c7d44b82d095c3a3aab17e5f793aa066eb8e
Rufian55 <chriskearns@rigidbody.tech>
2021-08-15 13:50:51 -0500

    Final...

--- BlackHoleRotator.cs
+++ BlackHoleRotator.cs
@@ -4,7 +4,7 @@
 using UnityEngine;
 
 public class BlackHoleRotator : MonoBehaviour {
-    
+
     #pragma warning disable 0649
     [SerializeField] private GameObject coreAssembly;
     [SerializeField] private float rotationSpeed;
